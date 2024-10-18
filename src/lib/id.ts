4cf2d1c1cc0146a32da2fc567c10f45912f8950a
grey275 <greytwosevenfive@gmail.com>
2024-10-18 09:12:51 -0700

    improve createId

--- src/lib/id.ts
+++ src/lib/id.ts
@@ -4,17 +4,27 @@ if (characters.length !== 64) {
 }
 
 /**
- * Generates a URL-safe random ID with 64 bits of entropy per character.
+ * Generates a URL-safe random ID with 6 bits of entropy per character. Why not just use base64 encoding? Because I like including ids in slugs sometimes.
  */
 export function createId(size: number) {
-	let result = ''
-	const arr = new Uint8Array(size)
+	const result: string[] = []
+	const arr = new Uint8Array(Math.ceil(size * (6 / 8)))
 	crypto.getRandomValues(arr)
 
+	let excess = 0
 	for (let i = 0; i < arr.length; i++) {
-		// we're wasting entropy here but it's not a big deal
-		result += characters[arr[i] % 64]
+		const entropy = arr[i]
+		// we have two extra bits per character on every uint8, so save them here and append them once we accumulate 6 bits of entropy
+		excess += entropy % 4 << ((i % 3) * 2)
+		if (i % 3 === 0) {
+			result.push(characters[excess])
+			if (result.length === size) return result.join('')
+			excess = 0
+		}
+
+		result.push(characters[entropy >> 2])
+		if (result.length === size) return result.join('')
 	}
 
-	return result
+	throw new Error('ran out of entropy somehow')
 }
