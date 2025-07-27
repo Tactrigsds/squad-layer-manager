99aeba9aac47fe0097d0d394572c3e63084fc7e2
grey275 <greytwosevenfive@gmail.com>
2025-07-27 15:36:47 -0700

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,15 @@
 import react from '@vitejs/plugin-react'
 import fs from 'node:fs'
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import * as AR from './src/app-routes.ts'
 import { ensureEnvSetup } from './src/server/env.ts'
 import * as Env from './src/server/env.ts'
 
-const prod = process.env.NODE_ENV === 'production'
-
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: !prod ? buildProxy() : undefined,
-		https: {
-			key: fs.readFileSync(path.resolve('.', 'certs/localhost-key.pem')),
-			cert: fs.readFileSync(path.join('.', 'certs/localhost.pem')),
-		},
-		headers: {
-			// required for sqlocal
-			'Cross-Origin-Embedder-Policy': 'require-corp',
-			'Cross-Origin-Opener-Policy': 'same-origin',
-		},
-	},
+	server: process.env.NODE_ENV === 'production' ? buildDevServer() : undefined,
 	envPrefix: 'PUBLIC_',
 	build: {
 		sourcemap: true,
@@ -38,10 +25,10 @@ export default defineConfig({
 	},
 })
 
-function buildProxy() {
+function buildDevServer(): UserConfig['server'] {
 	ensureEnvSetup()
 	const ENV = Env.getEnvBuilder({ ...Env.groups.httpServer })()
-	return Object.fromEntries(
+	const proxy = Object.fromEntries(
 		Object.values(AR.routes).map((r) => {
 			const protocol = r.websocket ? 'ws://' : 'http://'
 			const target = `${protocol}${ENV.HOST}:${ENV.PORT}`
@@ -55,4 +42,16 @@ function buildProxy() {
 			]
 		}),
 	)
+	return {
+		proxy,
+		https: {
+			key: fs.readFileSync(path.resolve('.', 'certs/localhost-key.pem')),
+			cert: fs.readFileSync(path.join('.', 'certs/localhost.pem')),
+		},
+		headers: {
+			// required for sqlocal
+			'Cross-Origin-Embedder-Policy': 'require-corp',
+			'Cross-Origin-Opener-Policy': 'same-origin',
+		},
+	}
 }
