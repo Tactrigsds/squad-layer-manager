4884c0396c0aab1eb11df0e4f1448f28814d6488
grey275 <greytwosevenfive@gmail.com>
2025-10-20 21:39:23 -0700

    Update 0032_gorgeous_oracle.sql

--- drizzle/0032_gorgeous_oracle.sql
+++ drizzle/0032_gorgeous_oracle.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `users` ADD `nickname` varchar(64);--> statement-breakpoint
+-- commenting this out for now as it's destructive and we may need to roll-back this release
 -- ALTER TABLE `users` DROP COLUMN `avatar`;
