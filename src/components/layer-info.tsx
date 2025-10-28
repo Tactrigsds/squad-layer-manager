a973161da03f08919f60b25a464d1bc2ffa66e14
grey275 <greytwosevenfive@gmail.com>
2025-10-28 02:30:08 -0700

    Adjust styles

--- src/components/layer-info.tsx
+++ src/components/layer-info.tsx
@@ -20,7 +20,7 @@ import { Navigate, useParams } from 'react-router-dom'
 import FullPageSpinner from './full-page-spinner.tsx'
 import MapLayerDisplay from './map-layer-display.tsx'
 import { Button, buttonVariants } from './ui/button.tsx'
-import { Dialog, DialogContent, DialogTrigger } from './ui/dialog.tsx'
+import { Dialog, DialogClose, DialogContent, DialogTrigger } from './ui/dialog.tsx'
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover.tsx'
 import { Spinner } from './ui/spinner.tsx'
 import TabsList from './ui/tabs-list.tsx'
@@ -59,7 +59,7 @@ export default function LayerInfoDialog(props: LayerInfoProps) {
 			<DialogTrigger asChild>
 				{props.children}
 			</DialogTrigger>
-			<DialogContent className="w-auto max-w-full overflow-x-auto overflow-y-auto max-h-screen min-w-0 pb-2">
+			<DialogContent className="w-auto max-w-full overflow-x-auto overflow-y-auto max-h-screen min-w-0 p-8">
 				<LayerInfo layerId={props.layerId} tab={tab || 'details'} setTab={setTab} close={() => setOpen(false)} />
 			</DialogContent>
 		</Dialog>
