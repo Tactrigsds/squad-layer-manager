8ad9d6b4b10c32d2ca4476a6fb99ec79fd221388
grey275 <greytwosevenfive@gmail.com>
2025-12-19 20:52:27 -0800

    only display enabled servers

--- src/server/config.ts
+++ src/server/config.ts
@@ -185,7 +185,7 @@ export function getPublicConfig(wsClientId: string) {
 		layersVersion: LayerDb.layersVersion,
 		commands: CONFIG.commands,
 		commandPrefix: CONFIG.commandPrefix,
-		servers: CONFIG.servers.map((server): ServerEntry => ({
+		servers: CONFIG.servers.filter(s => s.enabled).map((server): ServerEntry => ({
 			id: server.id,
 			displayName: server.displayName,
 		})),
